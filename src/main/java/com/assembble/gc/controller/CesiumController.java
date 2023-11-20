fc8430a0a9f392a9fdbd3dc90dfa1ce6cdc80808
xee2000 <xee2000@naver.com>
2023-11-20 16:29:11 +0900

    쿼리수정 당일날짜에 대한 값이 1개라도 존재시 alarm값 danger로 출력

--- src/main/java/com/assembble/gc/controller/CesiumController.java
+++ src/main/java/com/assembble/gc/controller/CesiumController.java
@@ -6,18 +6,12 @@ import com.assembble.gc.mapper.SensorMapper;
 import com.assembble.gc.service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Controller
